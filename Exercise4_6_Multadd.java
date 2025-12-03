a440e6d742fe733f4885889385481830730e275f
250542008 <250542008@firat.edu.tr>
2025-12-03 17:52:44 +0300

    Update Exercise4_6_Multadd.java

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,3 +1,46 @@
+public class Multadd {
+
+    // 1. ADIM: Ana Program (Main)
+    public static void main(String[] args) {
+
+        System.out.println("--- Basit Test ---");
+        multadd(1.0, 2.0, 3.0);
+
+        System.out.println("\n--- Trigonometri Testi ---");
+        double a1 = 1.0;
+        double b1 = Math.sin(Math.PI / 4);
+        double c1 = Math.cos(Math.PI / 4) / 2.0;
+        multadd(a1, b1, c1);
+
+        System.out.println("\n--- Logaritma Testi ---");
+        multadd(1.0, Math.log10(10), Math.log10(20));
+
+        System.out.println("\n--- expSum Metodu Testi ---");
+        expSum(2.0);
+    }
+
+    // 2. ADIM: multadd Metodu
+    // Görevi: a * b + c işlemini yapıp yazdırmak.
+    public static void multadd(double a, double b, double c) {
+        double result = a * b + c;
+        System.out.println(result);
+    }
+
+    // 3. ADIM: expSum Metodu
+    public static void expSum(double x) {
+       
+
+        double a = x;
+        double b = Math.exp(-x);           
+        double c = Math.sqrt(1 - b);       
+
+        System.out.print("x = " + x + " için sonuç: ");
+        multadd(a, b, c);
+    }
+}
+
+
+
 public class Exercise4_6_Multadd {
 
     public static double multadd(double a, double b, double c) {
